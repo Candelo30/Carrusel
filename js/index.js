8bfe9bbb0c94b44efcad5de72066098116cb14ed
Candelo30 <luiscandelo30@gmail.com>
2024-01-21 20:14:17 -0500

    Añadiendo nuevos cambios

---
+++ js/index.js
@@ -0,0 +1,41 @@
+const 
+  BtnRight = document.querySelector("#right_slider"),
+  Btnleft = document.querySelector("#left_slider"),
+  slider = document.querySelector(".container-img"),
+  ImgSlider = document.querySelectorAll(".img");
+
+BtnRight.addEventListener('click', e=> moveRight());    
+Btnleft.addEventListener('click', e=> moveLeft());    
+SliderView.addEventListener('click', e=> SearchImg());    
+
+var operacion = 0,
+counter = 0,
+widthImg = 100 / ImgSlider.length;
+
+function moveRight () {
+  if (counter >= ImgSlider.length-1) {
+      operacion = 0;
+      counter = 0;
+      slider.style.transform = `translate(-${operacion}%)`;;
+      slider.style.transition = "all ease .6s";
+  }else {         
+    counter ++; 
+    operacion += widthImg;
+    slider.style.transform = `translate(-${operacion}%)`
+    slider.style.transition = "all ease .6s";
+  }
+};
+
+function moveLeft () {
+  counter--;
+  if (counter < 0) {
+    counter = ImgSlider.length-1;
+    operacion = widthImg * (ImgSlider.length-1)
+    slider.style.transform = `translate(-${operacion}%)`
+    slider.style.transition = "all ease .6s"; 
+} else {
+    operacion -= widthImg;  
+    slider.style.transform = `translate(-${operacion}%)`
+    slider.style.transition = "all ease .6s";
+}
+};
