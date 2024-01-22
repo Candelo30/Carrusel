b80902bc990509b9836b9bdf6d5cae6f468b25b4
Candelo30 <luiscandelo30@gmail.com>
2024-01-22 18:14:59 -0500

    Añadiendo funcionalidad a las ayudas

--- js/index.js
+++ js/index.js
@@ -15,12 +15,12 @@ function moveRight () {
   if (counter >= ImgSlider.length-1) {
       operacion = 0;
       counter = 0;
-      slider.style.transform = `translate(-${operacion}%)`;;
+      slider.style.transform = `translate(-${operacion}%)`;
       slider.style.transition = "all ease .6s";
   }else {         
     counter ++; 
     operacion += widthImg;
-    slider.style.transform = `translate(-${operacion}%)`
+    slider.style.transform = `translate(-${operacion}%)`;
     slider.style.transition = "all ease .6s";
   }
 };
@@ -30,11 +30,32 @@ function moveLeft () {
   if (counter < 0) {
     counter = ImgSlider.length-1;
     operacion = widthImg * (ImgSlider.length-1)
-    slider.style.transform = `translate(-${operacion}%)`
+    slider.style.transform = `translate(-${operacion}%)`;
     slider.style.transition = "all ease .6s"; 
 } else {
     operacion -= widthImg;  
-    slider.style.transform = `translate(-${operacion}%)`
+    slider.style.transform = `translate(-${operacion}%)`;
     slider.style.transition = "all ease .6s";
 }
 };
+
+
+
+// Seleccionar todos los elementos con clase "position"
+const indexImgElements = document.querySelectorAll(".position");
+
+// Añadir event listener a cada elemento
+indexImgElements.forEach((element, index) => {
+  element.addEventListener('click', () => {
+    counter = 0;
+    counter = index - 1;
+    if (counter === 7) {
+      moveRight(); 
+    }
+    operacion = widthImg * (index);
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "all ease .6s";
+  });
+});
+
+
